271803ea5077eae4e4c8a2125028bf342193441e
JY-LemongO <122505119+JY-LemongO@users.noreply.github.com>
2024-01-15 09:31:47 +0900

    Update 자릿수 더하기.cs

--- 프로그래머스/Lv.1/12931. 자릿수 더하기/자릿수 더하기.cs
+++ 프로그래머스/Lv.1/12931. 자릿수 더하기/자릿수 더하기.cs
@@ -4,7 +4,13 @@ public class Solution {
     public int solution(int n) {
         int answer = 0;
             int devide = n;
-            for (int i = 0; i < n; i++)
+        int count = 0;
+        while(n > 0)
+        {
+            n /= 10;
+            count++;
+        }
+            for (int i = 0; i < count; i++)
             {
                 int remain = devide % 10;
                 answer += remain;
