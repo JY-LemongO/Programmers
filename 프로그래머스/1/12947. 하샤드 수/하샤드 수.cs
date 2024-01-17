712e9fb3851ed53fd9c854128b16a4e1e29ed056
JY-LemongO <122505119+JY-LemongO@users.noreply.github.com>
2024-01-17 23:16:23 +0900

    [level 1] Title: 하샤드 수, Time: 0.16 ms, Memory: 30.8 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.cs
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.cs
@@ -1,19 +1,12 @@
 public class Solution {
     public bool solution(int x) {
         bool answer;
-            int origin = x;
-            int[] arry = new int[5];
-            int index = 0;
+        int devideNum = 0;
+            int origin = x;                        
             for (int i = 0; i < x; i = 0)
             {
-                arry[index] = x % 10;
-                index++;
+                devideNum += x % 10;                
                 x /= 10;
-            }
-            int devideNum = 0;
-            for(int i = 0; i < arry.Length; i++)
-            {
-                devideNum += arry[i];
             }
             answer = origin % devideNum == 0;
 
