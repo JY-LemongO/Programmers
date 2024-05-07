2926b52e518a9cfa5b105f6816c60b937f6e1390
JY-LemongO <122505119+JY-LemongO@users.noreply.github.com>
2024-05-07 11:31:38 +0900

    [level 2] Title: 예상 대진표, Time: 0.20 ms, Memory: 30.9 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.cs
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.cs
@@ -8,15 +8,15 @@ class Solution
 
 int round = n / 2;
 
-for(int i = 0; i < round; i++)
+while (true)
 {
     if (a % 2 == 1 && a + 1 == b)
-    break;
-else if (a % 2 == 0 && a - 1 == b)
-    break;    
+        break;
+    else if (a % 2 == 0 && a - 1 == b)
+        break;
 
-a = a % 2 == 0 ? a / 2 : (a + 1) / 2;
-b = b % 2 == 0 ? b / 2 : (b + 1) / 2;
+    a = a % 2 == 0 ? a / 2 : (a + 1) / 2;
+    b = b % 2 == 0 ? b / 2 : (b + 1) / 2;
 
     answer++;
 }
