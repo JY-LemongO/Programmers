045c9d12e40ca09c77d8be7dbd1cc3e6576d0a52
JY-LemongO <122505119+JY-LemongO@users.noreply.github.com>
2024-05-07 10:55:47 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.23 ms, Memory: 31 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.cs
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.cs
@@ -1,26 +1,29 @@
 public class Solution {
-    public int LCM(int a,int b)
-    {
-        int n =a;
-        int m =b;
-        int temp=0;
-        while(m>0)
-        {
-            temp =n%m;
-            n=m;
-            m=temp;
-        }
-        return (a*b)/n;
-    }
     public int solution(int[] arr) {
-        if (arr.Length == 1)
-    return arr[0];
-        
-        int answer = LCM(arr[0],arr[1]);
-        for(int i=2; i<arr.Length; i++)
-        {
-            answer = LCM(answer,arr[i]);
-        }
+    if (arr.Length == 1)
+        return arr[0];
+
+    int answer = LCM(arr[0], arr[1]);
+
+    if (arr.Length == 2)
         return answer;
+
+    for (int i = 2; i < arr.Length; i++)
+        answer = LCM(answer, arr[i]);
+
+    return answer;
+}
+
+public int LCM(int a, int b)
+{
+    int n = a;
+    int m = b;
+    while (m > 0)
+    {
+        int temp = n % m;
+        n = m;
+        m = temp;
     }
+    return a * b / n;
+}
 }
