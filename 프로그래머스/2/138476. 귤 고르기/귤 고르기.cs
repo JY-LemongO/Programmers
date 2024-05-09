0e93b2d58ce17828a68457f1fbc3b22a639ef128
JY-LemongO <122505119+JY-LemongO@users.noreply.github.com>
2024-05-09 23:26:08 +0900

    [level 2] Title: 귤 고르기, Time: 18.38 ms, Memory: 35.9 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.cs
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.cs
@@ -4,35 +4,23 @@ using System.Collections.Generic;
 public class Solution {
     public int solution(int k, int[] tangerine) {
         int answer = 0;
-
-Array.Sort(tangerine);
-
-int latestSize = tangerine[0];
-int sizeCount = 0;
-
-List<int> sizeCounts = new List<int>();
-
-for (int i = 0; i < tangerine.Length; i++)
-{                
-    if (tangerine[i] != latestSize)
-    {
-        sizeCounts.Add(sizeCount);
-        sizeCount = 0;
-        latestSize = tangerine[i];
-    }
-    sizeCount++;
-}
-sizeCounts.Add(sizeCount);
-sizeCounts.Sort();            
-
-for(int i = sizeCounts.Count - 1; i >= 0; i--)
-{
-    answer++;
-    k -= sizeCounts[i];
-    if (k <= 0)
-        break;
-}
-
-return answer;
+        Dictionary<int, int> size = new Dictionary<int, int>();
+        foreach(int t in tangerine)
+        {
+            if(!size.TryAdd(t, 1))
+            {
+                size[t]+=1;
+            }
+        }
+        List<int> count = new List<int>(size.Values);
+        count.Sort();
+        int kCount = 0;
+        int kIndex = count.Count-1;
+        while(k>kCount)
+        {
+            kCount+=count[kIndex--];
+            answer++;
+        }
+        return answer;
     }
 }
