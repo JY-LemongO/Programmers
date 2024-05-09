9914657d58fb49788695bb7b5444955910977e92
JY-LemongO <122505119+JY-LemongO@users.noreply.github.com>
2024-05-09 23:25:10 +0900

    [level 2] Title: 귤 고르기, Time: 28.72 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+
+public class Solution {
+    public int solution(int k, int[] tangerine) {
+        int answer = 0;
+
+Array.Sort(tangerine);
+
+int latestSize = tangerine[0];
+int sizeCount = 0;
+
+List<int> sizeCounts = new List<int>();
+
+for (int i = 0; i < tangerine.Length; i++)
+{                
+    if (tangerine[i] != latestSize)
+    {
+        sizeCounts.Add(sizeCount);
+        sizeCount = 0;
+        latestSize = tangerine[i];
+    }
+    sizeCount++;
+}
+sizeCounts.Add(sizeCount);
+sizeCounts.Sort();            
+
+for(int i = sizeCounts.Count - 1; i >= 0; i--)
+{
+    answer++;
+    k -= sizeCounts[i];
+    if (k <= 0)
+        break;
+}
+
+return answer;
+    }
+}
