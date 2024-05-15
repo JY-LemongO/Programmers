b051fa0e8b80c5c64286253a0b05344ea4ec65e7
JY-LemongO <122505119+JY-LemongO@users.noreply.github.com>
2024-05-16 02:08:45 +0900

    [Bronze V] Title: 사칙연산, Time: 64 ms, Memory: 5512 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cs
@@ -0,0 +1,11 @@
+string z = Console.ReadLine();
+string[] zz = z.Split();
+
+int a = int.Parse(zz[0]);
+int b = int.Parse(zz[1]);
+
+Console.WriteLine(a + b);
+Console.WriteLine(a - b);
+Console.WriteLine(a * b);
+Console.WriteLine(a / b);
+Console.WriteLine(a % b);
