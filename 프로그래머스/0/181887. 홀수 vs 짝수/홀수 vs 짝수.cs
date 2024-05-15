822de46a6d6695611a7db29867bf16bf4f78b7ba
JY-LemongO <122505119+JY-LemongO@users.noreply.github.com>
2024-05-16 00:33:18 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.27 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.cs
@@ -0,0 +1,22 @@
+using System;
+
+public class Solution {
+    public int solution(int[] num_list) {
+        int answer;
+
+int a = 0;
+int b = 0;
+
+for (int i = 0; i < num_list.Length; i++)
+{
+    if (i % 2 == 0)
+        a += num_list[i];
+    else
+        b += num_list[i];
+}
+
+answer = a < b ? b : a;
+
+return answer;
+    }
+}
