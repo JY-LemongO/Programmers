60249b2716d8017a64677f3ff5d138ff52f31926
JY-LemongO <122505119+JY-LemongO@users.noreply.github.com>
2024-05-21 00:53:34 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.19 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution {
+    public int solution(string my_string, string is_prefix) {
+        for(int i = 0; i < is_prefix.Length; i++)
+{
+    if (i >= my_string.Length|| my_string[i] != is_prefix[i])
+        return 0;
+}   
+
+return 1;
+    }
+}
