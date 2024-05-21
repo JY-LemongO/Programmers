74ffe771e390e23acb121f0f5e99fa60bda85725
JY-LemongO <122505119+JY-LemongO@users.noreply.github.com>
2024-05-21 10:57:25 +0900

    [level 0] Title: 부분 문자열 이어 붙여 문자열 만들기, Time: 0.34 ms, Memory: 31 MB -BaekjoonHub

--- 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.cs
+++ 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.cs
@@ -1,18 +1,17 @@
 using System;
+using System.Text;
 
 public class Solution {
     public string solution(string[] my_strings, int[,] parts) {
-        string answer = "";            
-
-for(int i = 0; i < parts.GetLength(0); i++)
+        var sb = new StringBuilder();
+for (int i = 0; i < parts.GetLength(0); ++i)
 {
-    for(int j = parts[i,0]; j <= parts[i,1]; j++)
-    {
-        answer += my_strings[i][j];
-    }
-}
-    
+    int start = parts[i, 0];
+    int end = parts[i, 1];
 
-return answer;
+    string subString = my_strings[i].Substring(start, end - start + 1);
+    sb.Append(subString);
+}
+return sb.ToString();
     }
 }
