e2985419c542bb11793b53a23688c2cc36dc4298
JY-LemongO <122505119+JY-LemongO@users.noreply.github.com>
2024-05-25 00:23:28 +0900

    [level 0] Title: 꼬리 문자열, Time: 0.39 ms, Memory: 31 MB -BaekjoonHub

---
+++ 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Text;
+
+public class Solution {
+    public string solution(string[] str_list, string ex) {
+        StringBuilder sb = new StringBuilder();
+
+foreach(string str in str_list)
+{
+    if (str.Contains(ex))
+        continue;
+
+    sb.Append(str);
+}
+
+return sb.ToString();
+    }
+}
