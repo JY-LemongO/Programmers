39aff26f3cc6fab4bc76be09c7597dccaa6cb95f
JY-LemongO <122505119+JY-LemongO@users.noreply.github.com>
2024-05-24 15:14:22 +0900

    [level 0] Title: 두 수의 연산값 비교하기, Time: 0.84 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Text;
+
+public class Solution {
+    public int solution(int a, int b) {
+        int left = int.Parse($"{a}{b}");
+        int right = 2 * a * b;
+
+        return Math.Max(left, right);
+    }
+}
