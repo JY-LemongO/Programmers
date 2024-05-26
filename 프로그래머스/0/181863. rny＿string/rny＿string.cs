e48a93aaf9549b9cc18b5c4f78fd2cade8b4e532
JY-LemongO <122505119+JY-LemongO@users.noreply.github.com>
2024-05-26 23:40:03 +0900

    [level 0] Title: rny_string, Time: 0.17 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/0/181863. rny＿string/rny＿string.cs
+++ 프로그래머스/0/181863. rny＿string/rny＿string.cs
@@ -4,19 +4,6 @@ using System.Text;
 public class Solution {
     public string solution(string rny_string)
         {
-            StringBuilder sb = new StringBuilder();
-
-            foreach(char c in rny_string)
-            {
-                if (c == 'm')
-                {
-                    sb.Append("rn");
-                    continue;
-                }
-
-                sb.Append(c);
-            }
-
-            return sb.ToString();
+            return rny_string.Replace("m", "rn");
         }
 }
