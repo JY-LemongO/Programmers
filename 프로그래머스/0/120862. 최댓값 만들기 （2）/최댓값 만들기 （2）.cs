9e96a63c6342b04b5ddda17e18107e5855c19fdb
JY-LemongO <122505119+JY-LemongO@users.noreply.github.com>
2024-05-26 23:16:10 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 1.60 ms, Memory: 31.1 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.cs
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.cs
@@ -3,11 +3,8 @@ using System;
 public class Solution {
     public int solution(int[] numbers)
         {
-            Array.Sort(numbers);
-
-            if (numbers[0] * numbers[1] > numbers[numbers.Length - 1] * numbers[numbers.Length - 2])
-                return numbers[0] * numbers[1];
-
-            return numbers[numbers.Length - 1] * numbers[numbers.Length - 2];
+           int maxLen = numbers.Length-1;
+        Array.Sort(numbers);
+        return (int)MathF.Max(numbers[0]*numbers[1], numbers[maxLen]*numbers[maxLen-1]);
         }
 }
