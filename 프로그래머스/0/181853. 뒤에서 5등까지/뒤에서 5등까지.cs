b72f756570823510c206fe01591b7311896bb540
JY-LemongO <122505119+JY-LemongO@users.noreply.github.com>
2024-05-26 23:54:27 +0900

    [level 0] Title: 뒤에서 5등까지, Time: 2.50 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.cs
+++ 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.cs
@@ -2,7 +2,14 @@ using System;
 using System.Linq;
 
 public class Solution {
-    public int[] solution(int[] num_list) {
-        return num_list.OrderBy(o => o).Take(5).ToArray();
-    }
+    public int[] solution(int[] num_list)
+        {
+            int[] answer = new int[5];
+
+            Array.Sort(num_list);
+            for (int i = 0; i < answer.Length; i++)
+                answer[i] = num_list[i];
+
+            return answer;
+        }
 }
