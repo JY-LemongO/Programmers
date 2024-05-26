8130c6df107c660dee5338f0b6606cebcc5a1b52
JY-LemongO <122505119+JY-LemongO@users.noreply.github.com>
2024-05-26 22:39:17 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.30 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.cs
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.cs
@@ -2,10 +2,18 @@ using System;
 using System.Text;
 
 public class Solution {
-    public int solution(string myString, string pat) {
-        myString = myString.Replace("A", "b")
-                           .Replace("B", "a");
+    public int solution(string myString, string pat)
+        {
+            StringBuilder sb = new StringBuilder();
 
-        return myString.Contains(pat.ToLower()) ? 1 : 0;  
-    }
+            foreach(char c in myString)
+            {
+                if (c == 'A')
+                    sb.Append('B');
+                else
+                    sb.Append('A');
+            }
+
+            return sb.ToString().Contains(pat) ? 1 : 0;            
+        }
 }
