e7c6904ce884df677c6b0c15d6f64798706dc41c
JY-LemongO <122505119+JY-LemongO@users.noreply.github.com>
2024-05-29 00:01:43 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 0.17 ms, Memory: 30.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class Solution {
+    public string[] solution(string my_string)
+        {
+            string[] str = my_string.Split(' ', StringSplitOptions.RemoveEmptyEntries);        
+
+
+
+        return str;
+        }
+}
