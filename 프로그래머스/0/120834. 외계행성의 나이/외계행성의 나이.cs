c76706b7211fbf75dcef1472df529e4b9942fff7
JY-LemongO <122505119+JY-LemongO@users.noreply.github.com>
2024-05-31 15:36:54 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.34 ms, Memory: 30.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Text;
+
+public class Solution {
+    public string solution(int age)
+        {
+            string answer = "";
+        while(age>0)
+        {
+
+            answer=(char)(age%10+97)+answer;
+            age/=10;
+        }
+        return answer;
+        }
+}
