a52bcaa91499f65a0c827c5b61d78ad64b618b8d
JY-LemongO <122505119+JY-LemongO@users.noreply.github.com>
2024-06-01 20:44:02 +0900

    [level 0] Title: 약수 구하기, Time: 1.33 ms, Memory: 30.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.cs
@@ -0,0 +1,10 @@
+
+using System;
+using System.Linq;
+
+public class Solution {
+    public int[] solution(int n) {
+        int[] answer = Enumerable.Range(1, n).Where(x => n % x == 0).ToArray();
+        return answer;
+    }
+}
