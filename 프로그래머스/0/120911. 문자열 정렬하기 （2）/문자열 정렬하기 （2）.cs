98d8af55c13515455bc5bb0a8fa5693df46ce5d6
JY-LemongO <122505119+JY-LemongO@users.noreply.github.com>
2024-06-01 21:11:53 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 1.48 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.cs
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.cs
@@ -5,19 +5,16 @@ public class Solution {
     public string solution(string my_string)
         {
             StringBuilder sb = new StringBuilder(my_string.ToLower());
-            
-            for(int i = 0; i < sb.Length - 1; i++)
-            {
-                int index = i;
-                for(int j = i + 1; j < sb.Length; j++)
-                {
-                    if (sb[index] > sb[j])
-                        index = j;
-                }
 
-                char temp = sb[i];
-                sb[i] = sb[index];
-                sb[index] = temp;
+            for (int i = 0; i < sb.Length - 1; i++)
+            {
+                if (sb[i] > sb[i + 1])
+                {
+                    char c = sb[i];
+                    sb[i] = sb[i + 1];
+                    sb[i + 1] = c;
+                    i = -1;
+                }
             }
 
             return sb.ToString();
