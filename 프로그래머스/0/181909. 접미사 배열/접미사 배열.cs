a4f4d6beac0e3575f367813ba475abae7afa3133
JY-LemongO <122505119+JY-LemongO@users.noreply.github.com>
2024-06-03 03:10:45 +0900

    [level 0] Title: 접미사 배열, Time: 0.28 ms, Memory: 31 MB -BaekjoonHub

--- 프로그래머스/0/181909. 접미사 배열/접미사 배열.cs
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.cs
@@ -1,16 +1,20 @@
-
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Text;
+
 public class Solution {
-    public string[] solution(string my_string) {
-        var answer = new List<string>();
-        string str = "";
-        for (int i = my_string.Length - 1; i >= 0; i--)
+    public string[] solution(string my_string)
         {
-            str = my_string[i] + str;
-            answer.Add(str);
+            StringBuilder sb = new StringBuilder(my_string);
+            string[] strArr = new string[my_string.Length];
+
+            for(int i = 0; i < my_string.Length; i++)
+            {
+                strArr[i] = sb.ToString();
+                sb.Remove(0, 1);                
+            }
+
+            Array.Sort(strArr);
+
+            return strArr;
         }
-        return answer.OrderBy(s=>s).ToArray();;
-    }
 }
