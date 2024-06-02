081433134fcac70ef023956f8b624f8b1af7c270
JY-LemongO <122505119+JY-LemongO@users.noreply.github.com>
2024-06-03 02:01:22 +0900

    [level 0] Title: 두 수의 합, Time: 1904.37 ms, Memory: 34.7 MB -BaekjoonHub

--- 프로그래머스/0/181846. 두 수의 합/두 수의 합.cs
+++ 프로그래머스/0/181846. 두 수의 합/두 수의 합.cs
@@ -3,27 +3,35 @@ using System.Text;
 
 public class Solution {
     public string solution(string a, string b) {
-        StringBuilder result = new StringBuilder();
-int carry = 0;
-int len1 = a.Length;
-int len2 = b.Length;
-int maxLength = Math.Max(len1, len2);
+        string shortedStr = a.Length > b.Length ? b : a;
+        string longerStr = a.Length > b.Length ? a : b;
 
-for (int i = 0; i < maxLength; i++)
-{
-    int digit1 = i < len1 ? a[len1 - 1 - i] - '0' : 0;
-    int digit2 = i < len2 ? b[len2 - 1 - i] - '0' : 0;
+        var sb = new StringBuilder();
+        int over = 0;
+        int i = 0;
+        for(; i < shortedStr.Length; ++i)
+        {
+            int numA = shortedStr[shortedStr.Length - 1 - i] - '0';
+            int numB = longerStr[longerStr.Length - 1 - i] - '0';
 
-    int sum = digit1 + digit2 + carry;
-    carry = sum / 10;
-    result.Insert(0, sum % 10);
-}
+            int sum = numA + numB + over;
+            over = sum / 10;
 
-if (carry > 0)
-{
-    result.Insert(0, carry);
-}
+            sb.Insert(0, (sum % 10).ToString());
+        }
 
-return result.ToString();
+        for(; i < longerStr.Length; ++i)
+        {
+            int numB = longerStr[longerStr.Length - 1 - i] - '0';
+            int sum = numB + over;
+            over = sum / 10;
+
+            sb.Insert(0, (sum % 10).ToString());
+        }
+
+        if(over > 0)
+            sb.Insert(0, over.ToString());
+
+        return sb.ToString();
     }
 }
