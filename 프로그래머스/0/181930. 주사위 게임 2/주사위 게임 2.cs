974216b459ee916d1007ce08697baf941c7f656a
JY-LemongO <122505119+JY-LemongO@users.noreply.github.com>
2024-06-03 17:43:35 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.18 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution {
+    public int solution(int a, int b, int c)
+        {
+            if (a == b && b == c && a == c)
+                return (a + b + c) * (a * a + b * b + c * c) * (a * a * a + b * b * b + c * c * c);
+            if (a == b || a == c || b == c)
+                return (a + b + c) * (a * a + b * b + c * c);            
+
+            return a + b + c;
+        }
+}
