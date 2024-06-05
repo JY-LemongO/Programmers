710128873f0f473ff4a6aa2aead4221effbbfea6
JY-LemongO <122505119+JY-LemongO@users.noreply.github.com>
2024-06-05 18:51:48 +0900

    [level 2] Title: 할인 행사, Time: 1.82 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.cs
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.cs
@@ -1,58 +1,43 @@
+
 using System;
 using System.Collections.Generic;
 
 public class Solution {
-    public int solution(string[] want, int[] number, string[] discount)
-        {
-            Dictionary<string, int> saleItem = new Dictionary<string, int>();
+    int m_totalCount = 0 ;
+    Dictionary<string, int> m_wantDic = new Dictionary<string, int>();
+    int m_answer = 0;
+    public int solution(string[] want, int[] number, string[] discount) {
 
-        for (int i = 0; i < 10; i++)
-        {
-            if (saleItem.ContainsKey(discount[i]))
-            {
-                saleItem[discount[i]]++;
-            }
-
-            else
-            {
-                saleItem.Add(discount[i], 1);
-            }
+        for(int i = 0 ; i < want.Length; i++){
+            string wantConsume = want[i];
+            int wantCount = number[i];
+            m_totalCount += wantCount;
+            m_wantDic.Add(wantConsume, wantCount );
         }
 
-        int answer = 0;
-        for (int i = 0; ; i++)
-        {
-            bool passCheck = true;
-            for (int j = 0; j < want.Length; j++)
-            {
-                if (!saleItem.ContainsKey(want[j]) || saleItem[want[j]] < number[j])
-                {
-                    passCheck = false;
-                    break;
-                }
-            }
 
-            if (passCheck)
-            {
-                answer++;
-            }
-
-            if (discount.Length <= i + 10)
-            {
-                break;
-            }
-            saleItem[discount[i]]--;
-            if (saleItem.ContainsKey(discount[i + 10]))
-            {
-                saleItem[discount[i + 10]]++;
-            }
-
-            else
-            {
-                saleItem.Add(discount[i + 10], 1);
-            }
+        //가장 단순한건 - 한번 i가 시작하면 계속해서 i를 올리면서 탐방하는거 해당 딕션에서 
+        for ( int i = 0 ; i <= discount.Length - m_totalCount ; i ++ ){
+            CheckConsume(m_wantDic, i, discount);
         }
 
-        return answer;
+        return m_answer;
+    }
+    void CheckConsume(Dictionary<string, int> _consumeDic, int _startIndex, string[] _discount){
+        Dictionary<string, int> insDic = new Dictionary<string, int>(_consumeDic);
+        //Console.WriteLine(_startIndex+"부터 시작 만족해야하는 물품 수" +m_totalCount);
+         for ( int i = _startIndex ; i < _startIndex + m_totalCount ; i ++ ){
+            string disCountConsume = _discount[i];
+             if(!insDic.ContainsKey(disCountConsume)){
+                 return;
+             }
+             insDic[disCountConsume] -= 1;
+             if(insDic[disCountConsume]<0){
+                 return;
+             }
+
         }
+       // Console.WriteLine(_startIndex+"는 가능");
+        m_answer += 1; 
+    }
 }
