beb8eda5bb4e3f7ca39384cf4ffffce85b90c0cc
JY-LemongO <122505119+JY-LemongO@users.noreply.github.com>
2024-06-07 13:47:32 +0900

    [level 2] Title: 행렬의 곱셈, Time: 2.18 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.cs
@@ -0,0 +1,27 @@
+using System;
+
+public class Solution {
+    public int[,] solution(int[,] arr1, int[,] arr2)
+        {
+            int row = arr1.GetLength(0);
+            int col = arr2.GetLength(1);
+
+            int[,] answer = new int[row, col];
+
+            for (int i = 0; i < row; i++)
+            {                
+                for(int j = 0; j < col; j++)
+                {                    
+                    for (int k = 0;  k < arr1.GetLength(1); k++)
+                    {
+                        int x = arr1[i, k];
+                        int y = arr2[k, j];
+                        
+                        answer[i, j] += x * y;
+                    }                    
+                }
+            }
+
+            return answer;
+        }
+}
