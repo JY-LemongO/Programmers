a96dbae6adc428610d0799ac6b86af119a68219b
JY-LemongO <122505119+JY-LemongO@users.noreply.github.com>
2024-06-08 17:16:25 +0900

    [level 2] Title: 의상, Time: 2.30 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+
+public class Solution {
+    public int solution(string[,] clothes)
+        {
+            int answer = 1;
+
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+
+            for(int i = 0; i < clothes.GetLength(0); i++)
+            {
+                if (dict.ContainsKey(clothes[i, 1]))
+                    dict[clothes[i, 1]]++;
+                else
+                    dict.Add(clothes[i, 1], 1);
+            }
+
+            foreach(int i in dict.Values)
+                answer *= i + 1;
+
+            return --answer;
+        }
+}
