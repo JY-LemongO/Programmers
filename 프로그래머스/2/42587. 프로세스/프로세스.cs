fb965a800eabd84ff2b7f564ea5810139090404b
JY-LemongO <122505119+JY-LemongO@users.noreply.github.com>
2024-06-10 15:21:04 +0900

    [level 2] Title: 프로세스, Time: 1.20 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.cs
+++ 프로그래머스/2/42587. 프로세스/프로세스.cs
@@ -1,32 +1,43 @@
-
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 
 public class Solution {
     public int solution(int[] priorities, int location)
-    {
-        int answer = 0;
-        Queue<KeyValuePair<int, int>> que = new Queue<KeyValuePair<int, int>>();
-        for(int i = 0; i < priorities.Length; i++)
         {
-            que.Enqueue(new KeyValuePair<int, int>(i, priorities[i]));
-        }
-        while(true)
-        {
-            int nMax = que.Max(x => x.Value);
-            var kv = que.Dequeue();
-            if (kv.Value == nMax)
+            int answer = 0;
+            
+            List<int> ints = new List<int>(priorities);
+
+            while (true)
             {
-                if (kv.Key == location) return answer + 1;
-                else
+                bool canProcess = true;
+                int temp = ints[0];
+                ints.RemoveAt(0);
+
+                // 우선순위가 높은 프로세스가 있으면 방금 꺼낸 건 맨 뒤로
+                foreach (int i in ints)
                 {
-                    answer++;
-                    continue;
+                    if (temp < i)
+                    {
+                        ints.Add(temp);
+                        canProcess = false;
+                        break;
+                    }                        
                 }
+
+                // 프로세스 실행 가능할 때
+                if (canProcess)
+                {
+                    // 실행했으니 카운트 +1
+                    answer++;                    
+                    if (location == 0) // 방금 꺼낸게 찾는 프로세스면 while문 종료
+                        break;                    
+                }
+
+                // 로케이션을 앞으로 당김. 0보다 작으면 다시 맨뒤로
+                location = location - 1 < 0 ? ints.Count - 1 : location - 1;                
             }
-            que.Enqueue(kv);
-        }            
-    }
+
+            return answer;
+        }
 }
