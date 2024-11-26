d7c691eea9b5af47953218097fe27332beec2120
JY-LemongO <122505119+JY-LemongO@users.noreply.github.com>
2024-11-27 00:27:38 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 1.30 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.cs
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.cs
@@ -3,35 +3,79 @@ using System.Collections.Generic;
 public class Solution {
     public string solution(string s) {
         string answer = "";
-            bool minus = false;            
-            int sum = 0;
-            List<int> list = new List<int>();
-            for (int i = 0; i < s.Length; i++)
-            {
-                if (s[i] == '-')
-                {
-                    minus = true;
-                    continue;
-                }                   
+
+int bestest = int.MinValue;
+int worstest = int.MaxValue;
+int multiplier = 1;
+
+int currentNum = 0;
+
+for (int i = 0; i < s.Length; i++)
+{
+    if (s[i] == '-')
+    {
+        multiplier = -1;
+        continue;
+    }
+    else
+    {
+        if(s[i] == ' ' || i == s.Length - 1)
+        {
+            if(i == s.Length - 1)
+    currentNum = currentNum * 10 + (s[i] - 48);
+            
+            currentNum *= multiplier;
+            if (currentNum >= bestest)
+                bestest = currentNum;
+            if (currentNum <= worstest)
+                worstest = currentNum;                        
+        }
+        else
+        {
+            currentNum = currentNum * 10 + (s[i] - 48);
+            continue;
+        }                    
+    }
+
+    multiplier = 1;
+    currentNum = 0;
+}
+
+return answer = $"{worstest} {bestest}";          
+        
+        
+        
+        
+//         string answer = "";
+//             bool minus = false;            
+//             int sum = 0;
+//             List<int> list = new List<int>();
+//             for (int i = 0; i < s.Length; i++)
+//             {
+//                 if (s[i] == '-')
+//                 {
+//                     minus = true;
+//                     continue;
+//                 }                   
                                    
-                if (char.IsDigit(s[i]))
-                {
-                    if (sum != 0)
-                        sum *= 10;
-                    sum += (s[i] - 48);
-                }          
+//                 if (char.IsDigit(s[i]))
+//                 {
+//                     if (sum != 0)
+//                         sum *= 10;
+//                     sum += (s[i] - 48);
+//                 }          
 
-                if (s[i] == ' ' || i == s.Length - 1)
-                {
-                    int multi = minus ? -1 : 1;
-                    list.Add(sum * multi);
-                    minus = false;
-                    sum = 0;
-                }
-            }
-            list.Sort();
-            answer = $"{list[0]} {list[list.Count - 1]}";
+//                 if (s[i] == ' ' || i == s.Length - 1)
+//                 {
+//                     int multi = minus ? -1 : 1;
+//                     list.Add(sum * multi);
+//                     minus = false;
+//                     sum = 0;
+//                 }
+//             }
+//             list.Sort();
+//             answer = $"{list[0]} {list[list.Count - 1]}";
 
-            return answer;
+//             return answer;
     }
 }
