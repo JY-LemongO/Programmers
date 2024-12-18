d3756e15723ac366d5cd3123186e12d495893421
JY-LemongO <122505119+JY-LemongO@users.noreply.github.com>
2024-12-18 20:30:17 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.50 ms, Memory: 31 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.cs
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.cs
@@ -4,33 +4,33 @@ class Solution
 {
     public int solution(int n) 
    {
-//         int m = -1;
-
-// int oneCount = 0;
-// string binary = Convert.ToString(n, 2);
-// foreach (char c in binary)
-//     if (c == '1')
-//         oneCount++;
-
-// for(int i = n+1; i < 1000000; i++)
-// {
-//     int oneCountI = 0;
-//     string binaryI = Convert.ToString(i, 2);
-//     foreach (char c in binaryI)
-//         if (c == '1')
-//             oneCountI++;
-
-//     if(oneCount == oneCountI)
-//     {
-//         m = i;
-//         break;
-//     }                    
-// }
-
-// return m;
         // n < m 
         // 2진수로 변환했을 때 1의 개수가 같다.
         // 위 두 조건을 만족하는 것들 중 가장 작은 수
+        int m = -1;
+
+int oneCount = 0;
+string binary = Convert.ToString(n, 2);
+foreach (char c in binary)
+    if (c == '1')
+        oneCount++;
+
+for(int i = n+1; i < 1000000; i++)
+{
+    int oneCountI = 0;
+    string binaryI = Convert.ToString(i, 2);
+    foreach (char c in binaryI)
+        if (c == '1')
+            oneCountI++;
+
+    if(oneCount == oneCountI)
+    {
+        m = i;
+        break;
+    }                    
+}
+
+return m;
         
         
         
@@ -55,32 +55,33 @@ class Solution
         
         
         
-        string resultN;
-            string resultB;
-            int b = n;
-            bool correct = false;
+        
+//         string resultN;
+//             string resultB;
+//             int b = n;
+//             bool correct = false;
             
-            while (!correct)
-            {
-                b++;
+//             while (!correct)
+//             {
+//                 b++;
 
-                resultN = Convert.ToString(n, 2);
-                resultB = Convert.ToString(b, 2);
+//                 resultN = Convert.ToString(n, 2);
+//                 resultB = Convert.ToString(b, 2);
 
-                int countN = 0;
-                int countB = 0;
+//                 int countN = 0;
+//                 int countB = 0;
 
-                for (int i = 0; i < resultB.Length; i++)
-                {
-                    if (resultB[i] == '1')
-                        countB++;
-                    if(i <= resultN.Length - 1)
-                        if (resultN[i] == '1')
-                            countN++;
-                }                
-                correct = countN == countB;
-            }
+//                 for (int i = 0; i < resultB.Length; i++)
+//                 {
+//                     if (resultB[i] == '1')
+//                         countB++;
+//                     if(i <= resultN.Length - 1)
+//                         if (resultN[i] == '1')
+//                             countN++;
+//                 }                
+//                 correct = countN == countB;
+//             }
 
-            return b;
+//             return b;
     }
 }
