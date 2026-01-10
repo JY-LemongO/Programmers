8bf98b322dfc9d2cb21b4bb18ad47c1afa3e253b
JY-LemongO <122505119+JY-LemongO@users.noreply.github.com>
2026-01-10 15:40:40 +0900

    [Bronze V] Title: 구구단, Time: 44 ms, Memory: 5840 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.cs
+++ 백준/Bronze/2739. 구구단/구구단.cs
@@ -7,18 +7,12 @@ class Program{
         var sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
 var sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
 
-string format = "{0} * {1} = {2}";
 int.TryParse(sr.ReadLine(), out int output);
-
-object[] foramtArgs = new object[3];
 
 for (int i = 1; i < 10; i++)
 {
-    foramtArgs[0] = output;
-    foramtArgs[1] = i;
-    foramtArgs[2] = output * i;
-
-    string googoo = string.Format(format, foramtArgs);
+    string googoo = $"{output} * {i} = {output * i}";
+    
     sw.WriteLine(googoo);
 }
 
