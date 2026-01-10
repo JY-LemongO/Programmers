ca7706b2b67b39754f76003391b3f7d46c5f0b37
JY-LemongO <122505119+JY-LemongO@users.noreply.github.com>
2026-01-10 15:57:50 +0900

    [Bronze V] Title: 개, Time: 44 ms, Memory: 5360 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Program{
+    static void Main(string[] args){
+                    var sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+            var sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+            string cat = @"|\_/|
+|q p|   /}
+( 0 )""""""\
+|""^""`    |
+||_/=\\__|";
+            sw.WriteLine(cat);            
+
+            sr.Close();
+            sw.Close();
+    }
+}
